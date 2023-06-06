4de20295dcf05deb3a35c18747fb5a1b93c89464
SergoGansta777 <sergo777ser777@gmail.com>
2023-06-06 21:57:29 +0300

    added surround extension

--- plugins/editor.lua
+++ plugins/editor.lua
@@ -71,6 +71,16 @@ return {
     opts = {},
   },
   {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  {
     "phaazon/hop.nvim",
     branch = "v2", -- optional but strongly recommended
     config = function()
